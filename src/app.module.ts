a03c813d95479de3fadab5ec5854c7c98bb8fd48
오송주 <dhthdwn7920@gmail.com>
2024-01-18 15:58:16 +0900

    DB연동 & Redis 연동

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,33 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { RedisModule } from '@liaoliaots/nestjs-redis';
+import { configDotenv } from 'dotenv';
+
+configDotenv();
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      port: 8080,
+      port: 3306,
       host: process.env.DB_HOST,
       username: process.env.DB_USER,
       password: process.env.DB_PW,
       database: process.env.DB_NAME,
-      entities: ['./**/entities/*.ts'],
+      entities: [__dirname + './**/entities/*.ts'],
       timezone: 'Asia/Seoul',
       synchronize: true,
-      logging: true,
+      logging: false,
+      autoLoadEntities: true,
+    }),
+    RedisModule.forRoot({
+      readyLog: true,
+      config: {
+        host: process.env.REDIS_HOST,
+        port: Number(process.env.REDIS_PORT),
+        password: process.env.REDIS_PW,
+      },
     }),
     UserModule,
   ],
